5328a87bc39505d4d60340afc546f578e964eea3
Pcha Anthony <pcha.anthony@gmail.com>
2014-09-21 14:45:40 +0800

    fixtures testing

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,11 +4,20 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
   fixtures :posts
+  
   test "Posts attributes must not be empty" do
 	post = Post.new
 	assert post.invalid?
 	assert post.errors[:title].any?
 	assert post.errors[:detail].any?
+	end
+	
+  test "Posts is not valid without a unique title" do
+    post = Post.new(:title 	=> posts(:ruby).title,
+                    :detail	=> "yyyzzz" )
+	assert !post.save
+	assert_equal "has already been taken", post.errors[:title].join('; ')
 	end
 end
