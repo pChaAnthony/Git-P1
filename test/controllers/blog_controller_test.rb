c869b54d3eb663e554e07176f65b143c1a344703
Pcha Anthony <pcha.anthony@gmail.com>
2014-09-23 00:23:34 +0800

    blog index

--- test/controllers/blog_controller_test.rb
+++ test/controllers/blog_controller_test.rb
@@ -3,6 +3,9 @@ require 'test_helper'
 class BlogControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
+	assert_select '#columns #side a' , :minimum => 4
+	assert_select '#main .entry' , 3
+	assert_select 'h3' , 'Programming Ruby 1.9'
     assert_response :success
   end
 
