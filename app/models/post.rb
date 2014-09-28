409aa854b99510305b21b21e2f28da9508205483
Pcha Anthony <pcha.anthony@gmail.com>
2014-09-28 14:38:36 +0800

    shopping cart

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,17 @@ class Post < ActiveRecord::Base
 	default_scope { order 'title' }
 	validates :title, :detail, :presence => true
 	validates :title, :uniqueness => true
+	
+	has_many :line_items
+	
+	before_destroy :ensure_not_referenced_by_any_line_item
+	# ensure there are no line item reference to this post
+	def ensure_not_referenced_by_any_line_item
+		if line_items.count.zero?
+			return true
+		else
+			error[:base] << "There are Cart Line Items exist"
+			return false
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :line_items
+
+  resources :carts
+
   get 'blog/index'
 
   resources :posts
